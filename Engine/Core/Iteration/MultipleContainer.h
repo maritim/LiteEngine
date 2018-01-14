b6635d0c5717a5a3424facab796de74832930b99
maritim <cristi.lambru16@gmail.com>
2018-01-14 17:28:16 +0200

    Add multiple container iteration

---
+++ Engine/Core/Iteration/MultipleContainer.h
@@ -0,0 +1,61 @@
+#ifndef MULTIPLECONTAINER_H
+#define MULTIPLECONTAINER_H
+
+#include <vector>
+
+#include "Core/Console/Console.h"
+
+/*
+ * This need to be specialized
+ * Did not expect to use this directly... 
+ * Do nothing and explicitly show an error.
+*/
+
+#define MULTIPLE_CONTAINER_TEMPLATE \
+public: \
+	template<typename T> \
+	typename std::vector<T>::iterator begin () \
+	{ \
+		Console::LogError ("Ranged based loop missing specialization"); \
+		throw "Ranged based loop missing specialization"; \
+	} \
+	template<typename T> \
+	typename std::vector<T>::iterator end () \
+	{ \
+		Console::LogError ("Ranged based loop missing specialization"); \
+		throw "Ranged based loop missing specialization"; \
+	}
+
+/*
+ *
+*/
+
+#define MULTIPLE_CONTAINER_SPECIALIZATION(type, class, container) \
+template<> \
+inline std::vector<type>::iterator class::begin<type> () \
+{ \
+	return container.begin (); \
+} \
+template<> \
+inline std::vector<type>::iterator class::end<type> () \
+{ \
+	return container.end (); \
+}
+
+/*
+ * Range based loop macro for iterating over multiple container controller
+ * by specifying type of the container.
+ *
+ * Restrictions: every two containers need to have different types
+*/
+
+// TODO: Fix scope
+
+#define for_each_type(type, name, container) \
+	std::vector<type>::iterator name##_it; \
+	type name; \
+	for (name##_it = (container).begin<type> (), name = *name##_it; \
+		name##_it != (container).end<type> (); \
+		name##_it ++, name = *name##_it)
+
+#endif
